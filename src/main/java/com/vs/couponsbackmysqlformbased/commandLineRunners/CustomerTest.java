fe4cce7ba9941a4fb6ca93057d19a12b05f522dd
sidorovichva <sidorovichva@gmail.com>
2021-06-17 11:56:33 +0300

    210617

--- src/main/java/com/vs/couponsbackmysqlformbased/commandLineRunners/CustomerTest.java
+++ src/main/java/com/vs/couponsbackmysqlformbased/commandLineRunners/CustomerTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 @Order(3)
 @RequiredArgsConstructor
 public class CustomerTest implements CommandLineRunner {
